f773f391de0cae600db4b57580e75f6fd039fb5b
Francisco <galvan.cfj@gmail.com>
2017-11-05 16:27:06 -0800

    Prueba de frecuencia finalizada

--- Simulacion/Pages/Inicio.xaml.cs
+++ Simulacion/Pages/Inicio.xaml.cs
@@ -1,12 +1,13 @@
-﻿using MathNet.Numerics.Distributions;
-using Simulacion.Clases;
+﻿using Simulacion.Clases;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Media;
 using WpfMath.Controls;
+
 
 namespace Simulacion.Pages
 {
@@ -21,6 +22,11 @@ namespace Simulacion.Pages
             GenerarListas();
             GenerarColumnas();
             sl_main.Value = 10;
+            for (int i = 1; i <= 30; i++)
+                cbx_nRangos.Items.Add(i);
+
+            cbx_nRangos.SelectedIndex = 3;
+
         }
             List<DemandaMensualRow> lista_demandamensual;
             List<FactorRow> lista_factores;
@@ -286,6 +292,7 @@ namespace Simulacion.Pages
                     
                 }
                 dg_numeros.Items.Refresh();
+                RealizarPruebaDeFrecuencia();
             }
 
             private void tbx_interaciones_TextChanged(object sender, TextChangedEventArgs e)
@@ -687,8 +694,6 @@ namespace Simulacion.Pages
                 tbx_Q.Text = "100";
                 tbx_R.Text = "120";
                 tbx_invInicial.Text = "200";
-                tbx_nRangos.Text = "4";
-
                 dg_demandamensual.Items.Refresh();
                 dg_factores.Items.Refresh();
                 dg_tiempoEntrega.Items.Refresh();
@@ -787,19 +792,12 @@ namespace Simulacion.Pages
             MainWindow.navegador.Navigate(new Ajustes());
         }
 
-        private void tbx_nRangos_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (ValidarCampo((TextBox)sender))
-                RealizarPruebaDeFrecuencia();
-            
-        }
-
         private void RealizarPruebaDeFrecuencia()
         {
             if (lista_numeros.Count > 0)
             {
                 wrp_frecuencia.Children.Clear();
-                int NumeroDeRangos = int.Parse(tbx_nRangos.Text);
+                int NumeroDeRangos = int.Parse(cbx_nRangos.SelectedValue.ToString());
                 PruebaFrecuencia P1 = new PruebaFrecuencia(lista_numeros, NumeroDeRangos);
                 List<Rango> listaRangos = P1.CalcularRangos();
                 dg_rangos.ItemsSource = listaRangos;
@@ -816,21 +814,76 @@ namespace Simulacion.Pages
                 if(listaRangos.Count>0)
                 ((FormulaControl)wrp_frecuencia.Children[wrp_frecuencia.Children.Count - 1]).Formula += " = " + Math.Round(xo, 2);
 
-                if(stkp_frecuencia.Children.Count>10) 
-                    stkp_frecuencia.Children.RemoveRange(10, stkp_frecuencia.Children.Count-10);
+                if(stkp_frecuencia.Children.Count>9) 
+                    stkp_frecuencia.Children.RemoveRange(9, stkp_frecuencia.Children.Count-9);
 
-                    stkp_frecuencia.Children.Add(new FormulaControl() { Formula = " X_{0}^2 = " + Math.Round(xo, 2), Margin = new Thickness(5) });
-                if (ValidarCampo(tbx_alpha))
+                    stkp_frecuencia.Children.Add(new FormulaControl() { Formula = " X_{0}^2 = " + Math.Round(xo, 2), Margin = new Thickness(5), HorizontalContentAlignment=HorizontalAlignment.Center });
+                if (cbx_alpha.SelectedIndex >-1)
                 {
                     try
                     {
-                        ChiSquared c = new ChiSquared(double.Parse(tbx_nRangos.Text)-1);
-                        stkp_frecuencia.Children.Add(new FormulaControl() { Formula = " X^2 = (" + tbx_alpha.Text + "," + (int.Parse(tbx_nRangos.Text) - 1) + ") = " + Math.Round(c.InverseCumulativeDistribution(double.Parse(tbx_alpha.Text)), 4), Margin = new Thickness(5) });
-                    } catch { }              
+                        double x2 = Math.Round(double.Parse(obtenerChicuadrado(int.Parse(cbx_nRangos.SelectedValue.ToString()) - 1, cbx_alpha.SelectedIndex)), 4);
+                        stkp_frecuencia.Children.Add(new FormulaControl() { Formula = " X^2(" + cbx_alpha.SelectedValue.ToString() + "," + (int.Parse(cbx_nRangos.SelectedValue.ToString()) - 1) + ") = " + x2, Margin = new Thickness(5), HorizontalContentAlignment = HorizontalAlignment.Center });
+                        if (xo <= x2)
+                        {
+                            stkp_frecuencia.Children.Add(new FormulaControl() { Formula = "X_{0}^2 <= X^2", HorizontalContentAlignment = HorizontalAlignment.Center });
+                            stkp_frecuencia.Children.Add(new Viewbox() { Child = new TextBlock() { Text = "Los numeros estan distribuidos uniformemente",Foreground= Brushes.DarkGreen }, Margin = new Thickness(20) });
+                        }                          
+                        else
+                        {
+                            stkp_frecuencia.Children.Add(new FormulaControl() { Formula = "X_{0}^2 >= X^2", HorizontalContentAlignment = HorizontalAlignment.Center });
+                            stkp_frecuencia.Children.Add(new Viewbox() { Child = new TextBlock() { Text = "Los numeros NO estan distribuidos uniformemente" ,Foreground = Brushes.DarkRed } ,Margin=new Thickness(20)});
+                        }
+                           
+
+                        
+                        } catch (Exception e){ MessageBox.Show(e.ToString()); }              
                    
                 }
+
+                
                     
             }
+        }
+
+        private string obtenerChicuadrado(int GradosDeLibertad,int Probabilidad)
+        {
+            string[][] result =
+           (@"df  0.995	  0.99	 0.975	 0.95	 0.90	 0.10	 0.05	 0.025	 0.01	 0.005 \n" +
+            "1      0        0   0.001   0.004   0.016   2.706   3.841   5.024   6.635   7.879 \n" +
+            "2   0.010   0.020   0.051   0.103   0.211   4.605   5.991   7.378   9.210   10.597 \n" +
+            "3   0.072   0.115   0.216   0.352   0.584   6.251   7.815   9.348   11.345  12.838 \n" +
+            "4   0.207   0.297   0.484   0.711   1.064   7.779   9.488   11.143  13.277  14.860 \n" +
+            "5   0.412   0.554   0.831   1.145   1.610   9.236   11.070  12.833  15.086  16.750 \n" +
+            "6   0.676   0.872   1.237   1.635   2.204   10.645  12.592  14.449  16.812  18.548 \n" +
+            "7   0.989   1.239   1.690   2.167   2.833   12.017  14.067  16.013  18.475  20.278 \n" +
+            "8   1.344   1.646   2.180   2.733   3.490   13.362  15.507  17.535  20.090  21.955 \n" +
+            "9   1.735   2.088   2.700   3.325   4.168   14.684  16.919  19.023  21.666  23.589 \n" +
+            "10  2.156   2.558   3.247   3.940   4.865   15.987  18.307  20.483  23.209  25.188 \n" +
+            "11  2.603   3.053   3.816   4.575   5.578   17.275  19.675  21.920  24.725  26.757 \n" +
+            "12  3.074   3.571   4.404   5.226   6.304   18.549  21.026  23.337  26.217  28.300 \n" +
+            "13  3.565   4.107   5.009   5.892   7.042   19.812  22.362  24.736  27.688  29.819 \n" +
+            "14  4.075   4.660   5.629   6.571   7.790   21.064  23.685  26.119  29.141  31.319 \n" +
+            "15  4.601   5.229   6.262   7.261   8.547   22.307  24.996  27.488  30.578  32.801 \n" +
+            "16  5.142   5.812   6.908   7.962   9.312   23.542  26.296  28.845  32.000  34.267 \n" +
+            "17  5.697   6.408   7.564   8.672   10.085  24.769  27.587  30.191  33.409  35.718 \n" +
+            "18  6.265   7.015   8.231   9.390   10.865  25.989  28.869  31.526  34.805  37.156 \n" +
+            "19  6.844   7.633   8.907   10.117  11.651  27.204  30.144  32.852  36.191  38.582 \n" +
+            "20  7.434   8.260   9.591   10.851  12.443  28.412  31.410  34.170  37.566  39.997 \n" +
+            "21  8.034   8.897   10.283  11.591  13.240  29.615  32.671  35.479  38.932  41.401 \n" +
+            "22  8.643   9.542   10.982  12.338  14.041  30.813  33.924  36.781  40.289  42.796 \n" +
+            "23  9.260   10.196  11.689  13.091  14.848  32.007  35.172  38.076  41.638  44.181 \n" +
+            "24  9.886   10.856  12.401  13.848  15.659  33.196  36.415  39.364  42.980  45.559 \n" +
+            "25  10.520  11.524  13.120  14.611  16.473  34.382  37.652  40.646  44.314  46.928 \n" +
+            "26  11.160  12.198  13.844  15.379  17.292  35.563  38.885  41.923  45.642  48.290 \n" +
+            "27  11.808  12.879  14.573  16.151  18.114  36.741  40.113  43.195  46.963  49.645 \n" +
+            "28  12.461  13.565  15.308  16.928  18.939  37.916  41.337  44.461  48.278  50.993 \n" +
+            "29  13.121  14.256  16.047  17.708  19.768  39.087  42.557  45.722  49.588  52.336 \n" +
+            "30  13.787  14.953  16.791  18.493  20.599  40.256  43.773  46.979  50.892  53.672 \n")
+            .Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => x.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)).ToArray();
+           
+            return result[GradosDeLibertad][Probabilidad].ToString();
         }
 
         private void sl_main_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
@@ -839,14 +892,22 @@ namespace Simulacion.Pages
             bloque.Text = "Max: "+scv_main.ScrollableHeight.ToString()+ "ACT: "+sl_main.Value.ToString();
         }
 
-        private void tbx_formula_TextChanged(object sender, TextChangedEventArgs e)
+        private void cbx_nRangos_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            pizarron.Formula = tbx_formula.Text;
+            try
+            {
+                RealizarPruebaDeFrecuencia();
+            }
+            catch { }
         }
 
-        private void tbx_alpha_TextChanged(object sender, TextChangedEventArgs e)
+        private void cbx_alpha_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            try
+            {
+                RealizarPruebaDeFrecuencia();
+            }
+            catch { }
         }
     }
 }
